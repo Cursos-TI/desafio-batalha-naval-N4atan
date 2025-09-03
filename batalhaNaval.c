7d30e58fb70653d2c6c2f1530989504f053368e5
Natan <162522588+N4atan@users.noreply.github.com>
2025-09-03 13:07:04 +0000

    Nível Mestre - Finalizado

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,81 +1,68 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
-
-int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
-    int tabuleiro[10][10] = {
-        { 0, 3, 0, 0, 0, 0, 0, 0, 0, 0},
-        { 0, 3, 0, 0, 0, 0, 0, 0, 0, 0},
-        { 0, 3, 0, 0, 0, 0, 0, 0, 0, 0},
-        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        { 0, 0, 0, 0, 0, 0, 3, 3, 3, 0},
-        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-    };
-
-    
-    
-    // Imprimindo o Cabeçalho (A - J)
+void printTabuleiro(int size, int matriz[size][size]){
+    // Cabeçalho A, B, C, ...
     printf("   ");
-    for(int i = 0; i < 10; i++)
-    {   
-        printf(" %c", 'A' + i);
+    for(int c = 0; c < size; c++) {   
+        printf(" %c", 'A' + c);
     }
     printf("\n");
 
-    /*
-        A letra 'A' tem o valor 65.
-        Se você faz 'A' + i, o compilador soma i a esse número.
-    */
-
-
-    //i => Linha
-    //j => Coluna
-    for(int i = 0; i < 10; i++)
-    {
-        printf("%d | ", i);
-        for(int j = 0; j < 10; j++)
-        {
-            printf("%d ", tabuleiro[i][j]);
+    // Corpo
+    for(int i = 0; i < size; i++) {
+        printf("%2d | ", i+1); // número da linha
+        for(int j = 0; j < size; j++) {
+            printf("%d ", matriz[i][j]);
         }
         printf("\n");
     }
+    printf("\n");
+}
 
+int main() {
+    int size = 10;
+    int tabuleiro[10][10] = {0};
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+    // Exemplo: navio vertical
+    tabuleiro[0][1] = tabuleiro[1][1] = tabuleiro[2][1] = 3;
+    // Exemplo: navio horizontal
+    tabuleiro[7][6] = tabuleiro[7][7] = tabuleiro[7][8] = 3;
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+    printf("=== Tabuleiro com navios ===\n");
+    printTabuleiro(size, tabuleiro);
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+    // Agora Nível Mestre: matrizes de habilidades
+    int cone[5][5] = {0};
+    int cruz[5][5] = {0};
+    int octaedro[5][5] = {0};
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+    // Cone
+    cone[0][2] = 1;
+    cone[1][1] = cone[1][2] = cone[1][3] = 1;
+    cone[2][0] = cone[2][1] = cone[2][2] = cone[2][3] = cone[2][4] = 1;
+
+    // Cruz
+    cruz[0][2] = 1;
+    cruz[1][2] = 1;
+    cruz[2][0] = cruz[2][1] = cruz[2][2] = cruz[2][3] = cruz[2][4] = 1;
+    cruz[3][2] = 1;
+    cruz[4][2] = 1;
+
+    // Octaedro
+    octaedro[0][2] = 1;
+    octaedro[1][1] = octaedro[1][2] = octaedro[1][3] = 1;
+    octaedro[2][2] = 1;
+    octaedro[3][1] = octaedro[3][2] = octaedro[3][3] = 1;
+    octaedro[4][2] = 1;
+
+    printf("=== Cone ===\n");
+    printTabuleiro(5, cone);
+
+    printf("=== Cruz ===\n");
+    printTabuleiro(5, cruz);
+
+    printf("=== Octaedro ===\n");
+    printTabuleiro(5, octaedro);
 
     return 0;
 }
